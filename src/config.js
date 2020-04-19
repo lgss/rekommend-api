7c5815457c9337579ae1d35093f1ab0d579fd9cb
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-04-19 22:13:35 +0100

    Add disclaimer and positive outcome  functions

--- src/config.js
+++ src/config.js
@@ -13,3 +13,27 @@ exports.setGeneral = (event, context, callback) => {
 exports.loadGeneral = (event, context, callback) => {
   db.get_item("GENERAL", callback)
 }
+
+exports.setDisclaimer = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  db.simple_create(null, {
+    id: "DISCLAIMER",
+    content: body.content,
+  }, callback)
+}
+
+exports.loadDisclaimer = (event, context, callback) => {
+  db.get_item("DISCLAIMER", callback)
+}
+
+exports.setPositiveOutcome = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  db.simple_create(null, {
+    id: "POSITIVE",
+    content: body.content,
+  }, callback)
+}
+
+exports.loadPositiveOutcome = (event, context, callback) => {
+  db.get_item("POSITIVE", callback)
+}
