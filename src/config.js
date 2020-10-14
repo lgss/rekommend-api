ebc053f7d7106dec93f7e9e808dfc32eddbb9951
Scott Phillips <30724358+ScottyTooHotty@users.noreply.github.com>
2020-10-14 13:16:01 +0100

    Created api for Notices to use for feedback survey

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const db = require('./db');
+const { createResponse } = require('./util');
 
 exports.setContent = (event, context, callback) => {
   const body = JSON.parse(event.body);
@@ -25,4 +26,25 @@ exports.setTheme = (event, context, callback) => {
 
 exports.loadTheme = (event, context, callback) => {
   db.get_item("THEME", callback)
+}
+
+exports.loadBanners = (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: 'BANNERS'
+    }
+  }
+  return db.dynamo.get(params).promise()
+    .then(data => {
+      if (data.Item)
+        return createResponse(200, JSON.stringify(data.Item.banners))
+      
+      return createResponse(404, "No banners")
+    })
+
+    .catch(err => {
+      console.error(err)
+      return createResponse(500, "There was an error")
+    })
 }
