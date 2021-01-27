783fe2b4a6db82e8223596a70723403553b225f9
James Pickett <james.pickett@cambridgeshire.gov.uk>
2021-01-27 17:49:27 +0000

    Added url checking functionality

--- editor/src/resource.js
+++ editor/src/resource.js
@@ -1,5 +1,7 @@
 const db = require('db');
-const {v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid')
+const https = require('https');
+const tableName = process.env.TABLE_NAME;
 
 exports.get = (event) => {
     if (event.pathParameters && event.pathParameters.resourceid)
@@ -20,4 +22,60 @@ exports.put = (event) => {
 
 exports.delete = (event) => {
     return db.delete_item(event.pathParameters.resourceid, db.sortkey.resource);
+}
+
+exports.check_url = async (event) => {
+    let decodeUrl = Buffer.from(event.pathParameters.url, 'base64').toString();
+    const response = await new Promise((resolve, reject) => {
+        const req = https.get(decodeUrl, function (res) {
+            resolve({
+                statusCode: 200,
+                body: JSON.stringify(res.statusCode)
+            });
+        });
+        req.on('error', (e) => {
+            reject({
+                statusCode: 500,
+                body: 'Error'
+            });
+        });
+    });
+    return response;
+}
+
+exports.check = async (event) => {
+
+    const transitionPromise = () => {
+        let tparams = {
+            TableName: tableName,
+            Key: {
+                id: event.pathParameters.resourceid,
+                sort: db.sortkey.resource
+            },
+            ProjectionExpression: "doc.moreInfoUrl"
+        };
+        return db.dynamo.get(tparams).promise()
+    }
+
+    return transitionPromise().then((data) => {
+
+        let urlToCheck = data.Item.doc.moreInfoUrl
+        console.log(`Checking url: "${urlToCheck}`)
+        return new Promise((resolve, reject) => {
+            const req = https.get(urlToCheck, function (res) {
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify(res.statusCode)
+                });
+            });
+            req.on('error', (e) => {
+                reject({
+                    statusCode: 500,
+                    body: 'Error'
+                });
+            });
+
+        })
+
+    })
 }
